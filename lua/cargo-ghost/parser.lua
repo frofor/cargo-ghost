d78f1654e23ed2813bf73cc20de61ecb652f3849
frofor <18nraywczifc@protonmail.com>
2025-10-03 23:40:29 +0200

    fix(parser): handle deps with no version

--- lua/cargo-ghost/parser.lua
+++ lua/cargo-ghost/parser.lua
@@ -3,45 +3,26 @@
 ---@field version string
 ---@field line integer
 
----@param lines string[]
+---@param line string
 ---@param linenr integer
----@return Dependency|nil, integer
-local function parse_dep(lines, linenr)
-	local line = lines[linenr]
+---@return Dependency|nil
+local function parse_dep(line, linenr)
 	local crate, version = line:match('^%s*([%w_%-]+)%s*=%s*"([^"]+)"')
 	if crate and version then
-		local dep = { name = crate, version = version, line = linenr - 1 }
-		return dep, linenr + 1
+		return { name = crate, version = version, line = linenr - 1 }
 	end
 
 	crate = line:match('^%s*([%w_%-]+)%s*=%s*{')
 	if not crate then
-		return nil, linenr + 1
-	end
-
-	if not line:match('version%s*=') then
-		while true do
-			linenr = linenr + 1
-			if linenr == #lines then
-				return nil, linenr
-			end
-
-			line = lines[linenr]
-			if line:match('version%s*=') then
-				break
-			elseif line:match('}') then
-				return nil, linenr + 1
-			end
-		end
+		return nil
 	end
 
 	version = line:match('version%s*=%s*"([^"]+)"')
 	if not version then
-		return nil, linenr + 1
+		return nil
 	end
 
-	local dep = { name = crate, version = version, line = linenr - 1 }
-	return dep, linenr + 1
+	return { name = crate, version = version, line = linenr - 1 }
 end
 
 ---@param buf integer
@@ -51,9 +32,7 @@ local function parse_cargo_toml(buf)
 	local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
 	local in_deps = false
 
-	local linenr = 1
-	while linenr <= #lines do
-		local line = lines[linenr]
+	for linenr, line in ipairs(lines) do
 		if line:match('^%s*%[dependencies')
 			or line:match('^%s*%[dev-dependencies')
 			or line:match('^%s*%[build-dependencies')
@@ -64,15 +43,13 @@ local function parse_cargo_toml(buf)
 		end
 
 		if not in_deps then
-			linenr = linenr + 1
 			goto continue
 		end
 
-		local dep, new_linenr = parse_dep(lines, linenr)
+		local dep = parse_dep(line, linenr)
 		if dep then
 			table.insert(deps, dep)
 		end
-		linenr = new_linenr
 
 		::continue::
 	end
